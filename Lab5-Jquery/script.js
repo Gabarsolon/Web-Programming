73dbb1a886322b7cd46de47eb87a910e06001e05
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-04-05 12:55:52 +0300

    ii buna tema gata

--- Lab5-Jquery/script.js
+++ Lab5-Jquery/script.js
@@ -1,22 +1,27 @@
-jQuery(function(){
-    $("#button-main-form").on("click", function(){
-        $("#modal-window").css("display", "block");
+jQuery(function () {
+    $("#button-main-form").on("click", function () {
+        $("#modal-window, #overlay").css("display", "block");
+        $("#main-form :input, #button-main-form").prop("disabled", true);
     });
 
-    $("#button-modal-form").on("click", function(){
+    $("#button-modal-form").on("click", function () {
         $(this).parent().css("display", "none");
-        
+        $("#overlay").css("display", "none");
+
         let fieldToChangeID = "#name";
-        if(Math.floor(Math.random() * 2)){
+        if (Math.floor(Math.random() * 2)) {
             fieldToChangeID = "#league";
         }
 
         let fieldsConcat = "";
-        $("#modal-form :input").each(function(){
+        $("#modal-form :input").each(function () {
             fieldsConcat += $(this).val();
         });
-        
+
         $("#main-form").children(fieldToChangeID).val(fieldsConcat);
+        $("#modal-form").trigger("reset");
+
+        $("#main-form :input, #button-main-form").prop("disabled", false);
     });
 
 });
