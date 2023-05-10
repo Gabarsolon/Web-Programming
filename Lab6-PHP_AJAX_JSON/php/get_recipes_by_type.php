bb1cf112608a9fb864c6a4069a76edc88e179e85
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-05-10 18:47:16 +0300

    update

--- Lab6-PHP_AJAX_JSON/php/get_recipes_by_type.php
+++ Lab6-PHP_AJAX_JSON/php/get_recipes_by_type.php
@@ -1,45 +1,23 @@
 <?php
+	require_once 'recipe.php';
+
 	$con = mysqli_connect("localhost","root","", "food_recipes");
 	if (!$con) {
 		die('Could not connect: ' . mysql_error());
 	}
 
-	$type = $_GET["type"];
+	$type = mysqli_real_escape_string($con ,$_GET["type"]);
 
-	//mysql_query("insert into Students values(3,'aaa','pass',23)");
 	$result = mysqli_query($con, "SELECT * FROM Recipes where type LIKE '%" . $type . "%'");
-
-	//echo "<html><body>";
-	echo "<table border='1'>
-	<tr>
-		<th>Operations</th>
-		<th>ID</th>
-		<th>Author</th>
-		<th>Name</th>
-		<th>Type</th>
-		<th>Preparation Time</th>
-		<th>Servings</th>
-		<th>Ingredients</th>
-		<th>Method</th>
-	</tr>";
-
-	while($row = mysqli_fetch_array($result)){
-		echo "<tr>";
-		echo "<td>
-			<a href=\"update.html?id=" . $row['id'] . "\"><button>Update</button></a><br>		
-			<button id=\"remove-button\">Remove</button>
-			</td>";
-		echo "<td>" . $row['id'] . "</td>";
-		echo "<td>" . $row['author'] . "</td>";
-		echo "<td>" . $row['name'] . "</td>";
-		echo "<td>" . $row['type'] . "</td>";
-		echo "<td>" . $row['prep_time'] . "</td>";
-		echo "<td>" . $row['servings'] . "</td>";
-		echo "<td>" . $row['ingredients'] . "</td>";
-		echo "<td>" . $row['method'] . "</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
-	//echo "</body></html>";
 	mysqli_close($con);
+	
+	$recipes = array();
+	while($row = mysqli_fetch_array($result)){
+		$recipe = new Recipe($row['id'], $row['author'], $row['name'], $row['type'],
+			$row['prep_time'], $row['servings'], $row['ingredients'], $row['method']);
+		array_push($recipes, $recipe);
+	}
+	
+
+	echo json_encode($recipes);
 ?> 
---
+++ Lab6-PHP_AJAX_JSON/php/recipe.php
@@ -0,0 +1,30 @@
+<?php
+
+class Recipe implements JsonSerializable {
+	private $id;
+	private $author;
+	private $name;
+	private $type;	
+	private $prep_time;
+    private $servings;
+    private $ingredients;
+    private $method;
+
+	public function __construct($id, $author, $name, $type, $prep_time, $servings, $ingredients, $method) {
+		$this->id = $id;
+        $this->author = $author;
+        $this->name = $name;
+        $this->type = $type;
+        $this->prep_time = $prep_time;
+        $this->servings = $servings;
+        $this->ingredients = $ingredients;
+        $this->method = $method;
+	}
+
+	public function jsonSerialize() {
+        $vars = get_object_vars($this);
+        return $vars;
+    }
+}
+
+?>
