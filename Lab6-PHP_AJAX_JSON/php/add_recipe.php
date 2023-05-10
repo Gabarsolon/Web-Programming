51c5abc7581eecb31ca77ae925009bfd70751f18
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-05-11 01:08:35 +0300

    update

--- Lab6-PHP_AJAX_JSON/php/add_recipe.php
+++ Lab6-PHP_AJAX_JSON/php/add_recipe.php
@@ -1,26 +1,27 @@
 <?php 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: *");
+    header('Access-Control-Allow-Origin: *');
 
     $con = mysqli_connect("localhost", "root", "", "food_recipes");
     if(!$con){
         die('Could not connect: ' . mysql_error());
     }
     
-    $author = mysqli_real_escape_string($con, $_POST['author']);
-    $name = mysqli_real_escape_string($con, $_POST['name']);
-    $type = mysqli_real_escape_string($con, $_POST['type']);
-    $prep_time = mysqli_real_escape_string($con, $_POST['prep_time']);
-    $servings = mysqli_real_escape_string($con, $_POST['servings']);
-    $ingredients = mysqli_real_escape_string($con, $_POST['ingredients']);
-    $method = mysqli_real_escape_string($con, $_POST['method']);
+    $json = file_get_contents('php://input');
+    $recipe = json_decode($json, true);
+
+    $author = mysqli_real_escape_string($con, $recipe['author']);
+    $name = mysqli_real_escape_string($con, $recipe['name']);
+    $type = mysqli_real_escape_string($con, $recipe['type']);
+    $prep_time = mysqli_real_escape_string($con, $recipe['prep_time']);
+    $servings = mysqli_real_escape_string($con, $recipe['servings']);
+    $ingredients = mysqli_real_escape_string($con, $recipe['ingredients']);
+    $method = mysqli_real_escape_string($con, $recipe['method']);
 
 	$result = mysqli_query($con, 
         "INSERT INTO Recipes(author, name, type, prep_time, servings, ingredients, method)
         VALUES('$author', '$name', '$type', '$prep_time', '$servings', '$ingredients', '$method');" 
     );
-    
+
     echo "The recipe was added succesfully";
 
     mysqli_close($con);
