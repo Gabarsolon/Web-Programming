9057e17a0f11a219cdd6bfdccd51fcf0a320582b
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-06-04 23:39:19 +0300

    jeah

---
+++ Lab9-AspNet/AspMVCex/AspMVCex/Controllers/MainController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AspMVCex.Models;
+using AspMVCex.DataAbstractionLayer;
+
+namespace AspMVCex.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View("FilterRecipes");
+        }
+
+        public string TestController()
+        {
+            return "Testing the MainController .. OK!";
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View("AddNewStudent");
+        }
+
+        //public ActionResult SaveStudent()
+        //{
+        //    Recipe stud = new Recipe();
+        //    stud.Id = int.Parse(Request.Params["id"]);
+        //    stud.Nume = Request.Params["nume"];
+        //    stud.Password = Request.Params["password"];
+        //    stud.Group_id = int.Parse(Request.Params["group_id"]);
+
+        //    DAL dal = new DAL();
+        //    dal.SaveStudent(stud);
+        //    return RedirectToAction("GetStudents");
+        //}
+
+        public string GetRecipesByType()
+        {
+            string type = Request.Params["type"];
+            DAL dal = new DAL();
+            List<Recipe> recipeList = dal.GetRecipesByType(type);
+            ViewData["recipeList"] = recipeList;
+
+            string result = "" +
+                "<table>" +
+                    "<thead>" +
+                        "<th>Id</th>" +
+                        "<th>Author</th>" +
+                        "<th>Name</th>" +
+                        "<th>Type</th>" +
+                        "<th>Preparation Time</th>" + 
+                        "<th>Servings</th>" +
+                        "<th>Ingredients</th>" +
+                        "<th>Method</th>" +
+                    "</thead>";
+
+            foreach (Recipe recipe in recipeList)
+            {
+                result += 
+                    "<tr>" +
+                        "<td>" + recipe.Id + "</td>" +
+                        "<td>" + recipe.author + "</td>" +
+                        "<td>" + recipe.name + "</td>" +
+                        "<td>" + recipe.type + "</td>" +
+                        "<td>" + recipe.prep_time + "</td>" +
+                        "<td>" + recipe.servings + "</td>" +
+                        "<td>" + recipe.ingredients + "</td>" +
+                        "<td>" + recipe.method + "</td>" +
+                        "<td>" +
+                    "</tr>";
+            }
+
+            result += "</table>";
+            return result;
+        }
+    }
+}
---
+++ Lab9-AspNet/AspMVCex/AspMVCex/DataAbstractionLayer/DAL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using AspMVCex.Models;
+using MySql.Data.MySqlClient;
+
+namespace AspMVCex.DataAbstractionLayer
+{
+    public class DAL
+    {
+        public List<Recipe> GetRecipesByType(string type)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=food_recipes;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from recipes where type LIKE '%" + @type + "%'";
+                cmd.Parameters.AddWithValue("type", type);
+                MySqlDataReader myreader = cmd.ExecuteReader();
+
+                List<Recipe> recipes = new List<Recipe>();
+                while (myreader.Read())
+                {
+                    Recipe recipe = new Recipe();
+                    recipe.Id = myreader.GetInt32("id");
+                    recipe.author = myreader.GetString("author");
+                    recipe.name = myreader.GetString("name");
+                    recipe.type = myreader.GetString("type");
+                    recipe.prep_time = myreader.GetString("prep_time");
+                    recipe.servings = myreader.GetInt32("servings");
+                    recipe.ingredients = myreader.GetString("ingredients");
+                    recipe.method = myreader.GetString("method");
+                    recipes.Add(recipe);
+                }
+                myreader.Close();
+                return recipes;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return null;
+
+        }
+
+    }
+}
